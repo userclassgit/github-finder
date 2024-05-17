b07429dfed3dd02fceb8af0da19fb249e23e1022
Myeongjeon Lee <userclassdev@outlook.com>
2024-05-17 01:50:17 -0500

    update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import './css/reset.css'
-import './css/style.css'
-import Search from './components/Search'
-import User from './components/User'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './css/reset.css';
+import './css/style.css';
+import Search from './components/Search';
+import User from './components/User';
 
 function App() {
   return (
-    <div>
-      <Search />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Search />
+        </Route>
+        <Route path="/user/:username">
+          <User />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
