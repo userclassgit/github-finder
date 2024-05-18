8c19a463e68cc79c61f16a426dce9662b49e6e01
Myeongjeon Lee <userclassdev@outlook.com>
2024-05-18 11:40:40 -0500

    responsive

--- src/components/Repository.jsx
+++ src/components/Repository.jsx
@@ -4,9 +4,9 @@ const Repository = ({ repo }) => {
       <div className="repository-content">
         <div className="repository-top">
           <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
-            <h3>{repo.name}</h3>
+            <h3 className="repository-name">{repo.name}</h3>
           </a>
-          <p>Updated on {new Date(repo.updated_at).toLocaleDateString()}</p>
+          <p className="last-updated">Updated on {new Date(repo.updated_at).toLocaleDateString()}</p>
         </div>
         <p className="repository-description">{repo.description}</p>
       </div>
